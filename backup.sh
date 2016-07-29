004599b2aa2b74a7cdb5348d63f867073125df95
mewmew <rnd0x00@gmail.com>
2016-07-29 14:36:49 +0200

    backup: Use DST_DIR to specify the backup destination directory.

--- backup.sh
+++ backup.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-#export DST_DIR=/mnt/backup/laptop_`date +%F`
-export DST_DIR="${HOME}/backup/laptop_`date +%F`"
-mkdir -p "${DST_DIR}/"
+if [[ -z ${DST_DIR// } ]]; then
+	#export DST_DIR="/mnt/backup"
+	export DST_DIR="${HOME}/backup"
+fi
+export BACKUP_DIR="${DST_DIR}/laptop_`date +%F`"
+mkdir -p "${BACKUP_DIR}/"
 
 # Private keys.
 echo "~/.ssh"
-#rsync -r --delete-before --info=progress2 ".gnupg" "${DST_DIR}/"
-rsync -r --delete-before --info=progress2 ".ssh" "${DST_DIR}/"
+#rsync -r --delete-before --info=progress2 ".gnupg" "${BACKUP_DIR}/"
+rsync -r --delete-before --info=progress2 ".ssh" "${BACKUP_DIR}/"
 
 # Virtual box.
-#mkdir -p "${DST_DIR}/.config/"
+#mkdir -p "${BACKUP_DIR}/.config/"
 #echo "~/.config/VirtualBox"
-#rsync -r --delete-before --info=progress2 ".config/VirtualBox" "${DST_DIR}/.config/"
+#rsync -r --delete-before --info=progress2 ".config/VirtualBox" "${BACKUP_DIR}/.config/"
 #echo "~/VirtualBox VMs"
-#rsync -r --delete-before --info=progress2 "VirtualBox VMs" "${DST_DIR}/"
+#rsync -r --delete-before --info=progress2 "VirtualBox VMs" "${BACKUP_DIR}/"
 
 # Scripts.
 echo "~/bin"
-rsync -r --delete-before --info=progress2 "bin" "${DST_DIR}/"
+rsync -r --delete-before --info=progress2 "bin" "${BACKUP_DIR}/"
 
 # Desktop.
 echo "~/Desktop"
-rsync -r --delete-before --info=progress2 "Desktop" "${DST_DIR}/"
+rsync -r --delete-before --info=progress2 "Desktop" "${BACKUP_DIR}/"
 
 # Downloads.
 echo "~/dl"
-rsync -r --delete-before --info=progress2 "dl" "${DST_DIR}/"
+rsync -r --delete-before --info=progress2 "dl" "${BACKUP_DIR}/"
 
 # Wallpapers.
 echo "~/resources"
-rsync -r --delete-before --info=progress2 "resources" "${DST_DIR}/"
+rsync -r --delete-before --info=progress2 "resources" "${BACKUP_DIR}/"
