0e66e27478387c729cd246bc233b17b20e7603cb
mewmew <rnd0x00@gmail.com>
2016-12-22 11:15:58 +0100

    install: Add repo for infinity bundle to pacman.conf.

--- install/theme.sh
+++ install/theme.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+sudo bash -c "cat >> /etc/pacman.conf <<'_EOF'
+
+[infinality-bundle]
+Server = http://bohoomil.com/repo/$arch
+
+[infinality-bundle-fonts]
+Server = http://bohoomil.com/repo/fonts
+_EOF"
+
 sudo pacman -Sy \
 	elementary-icon-theme \
 	gtk-engine-murrine \
 	lxappearance \
 	ttf-hack \
+
 
 packer -S \
 	ibfonts-meta-base \
