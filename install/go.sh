c6cf4723b57af7b2b9224f117c2cd1297f1e249b
mewmew <rnd0x00@gmail.com>
2016-12-22 09:51:27 +0100

    install: Install Go 1.4 prior to Go tip.

--- install/go.sh
+++ install/go.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd ~
-git clone https://go.googlesource.com/go.git
-cd ~/go/src
+git clone https://go.googlesource.com/go.git ~/go
+# Install Go 1.4.
+cp -r ~/go ~/go1.4
+cd ~/go1.4/src
+git checkout release-branch.go1.4
+time ./all.bash
+# Install Go tip.
+cd ~/go
 time ./all.bash
