fdcb8b512542aeb0c3bc46c33d5cfd1dff82ba7c
mewmew <rnd0x00@gmail.com>
2016-12-22 12:52:39 +0100

    install: Fix name of goccmack.

--- install/go_get.sh
+++ install/go_get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 go get -u github.com/svent/sift
 go get -u github.com/sourcegraph/thyme
-go get -u github.com/goccmac/gocc
+go get -u github.com/goccmack/gocc
 go get -u golang.org/x/tools/...
 go get -u github.com/golang/lint/golint
