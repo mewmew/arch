d44769895c678cee92174884750e10e15e89a05a
mewmew <rnd0x00@gmail.com>
2017-01-20 11:55:21 +0100

    go_get: Add fzz.

--- install/go_get.sh
+++ install/go_get.sh
@@ -4,3 +4,4 @@ go get -u github.com/sourcegraph/thyme
 go get -u github.com/goccmack/gocc
 go get -u golang.org/x/tools/...
 go get -u github.com/golang/lint/golint
+go get -u github.com/mrnugget/fzz
