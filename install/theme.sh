4c8981f710ed0f73bfd628fccad91955162ad466
mewmew <rnd0x00@gmail.com>
2017-01-26 00:27:34 +0100

    install: Replace infinality bundle with default fonts.

--- install/theme.sh
+++ install/theme.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-sudo bash -c "grep -q infinality-bundle /etc/pacman.conf || cat >> /etc/pacman.conf <<'_EOF'
-
-[infinality-bundle]
-Server = http://bohoomil.com/repo/\$arch
-
-[infinality-bundle-fonts]
-Server = http://bohoomil.com/repo/fonts
-_EOF"
-
-sudo pacman-key --lsign-key 962DDE58
-
 sudo pacman -Sy \
 	elementary-icon-theme \
 	gtk-engine-murrine \
@@ -18,8 +7,17 @@ sudo pacman -Sy \
 	ttf-hack \
 
 packer -S \
-	ibfonts-meta-base \
-	ibfonts-meta-extended \
-	ibfonts-meta-extended-lt \
-	infinality-bundle \
 	vertex-themes-git \
+
+# infinality bundle replacement.
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+wget -O /tmp/local.conf https://gist.githubusercontent.com/cryzed/4f64bb79e80d619866ee0b18ba2d32fc/raw/bd073b52365393f9f0718425271825fc27b218f7/local.conf
+sudo mv /tmp/local.conf /etc/fonts/local.conf
+
+sudo pacman -S \
+	t1-cursor-ib \
+	ttf-gelasio-ib \
+
+sudo packer -S \
+	packer -S fonts-meta-extended-lt \
