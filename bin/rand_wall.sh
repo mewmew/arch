e3e701ea5f9db597c187b55beb54ad73f6239aaf
mewmew <rnd0x00@gmail.com>
2017-03-07 22:30:11 +0100

    bin: Add rand_wall.sh.

--- bin/rand_wall.sh
+++ bin/rand_wall.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-walls -n 1 -sorting random -purity SFW nature | xargs -I {} feh --bg-fill "{}"
+if ping -c 1 8.8.8.8 > /dev/null ; then
+	# Use remove wallpaper.
+	WALL=`walls -n 1 -sorting random -purity SFW nature`
+else
+	# Use local wallpaper.
+	WALL=`find /home/u/resources/ -maxdepth 2 -type f | shuf -n1`
+fi
+echo "${WALL}"
+feh --bg-fill "${WALL}"
---
+++ bin/rand_wall_loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while [ "$DISPLAY" == ":0" ]
+do
+	/home/u/bin/rand_wall.sh
+	sleep 1m
+done
+
+while [ "$DISPLAY" == ":0.0" ]
+do
+	/home/u/bin/rand_wall.sh
+	sleep 1m
+done
