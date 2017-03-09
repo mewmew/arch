de0dc9fd28a4129f811186ef0a4f916afd7462fe
mewmew <rnd0x00@gmail.com>
2017-03-09 12:29:50 +0100

    install: Add alternative deps for IDA as comments to ida.sh.

--- install/ida.sh
+++ install/ida.sh
@@ -3,3 +3,7 @@ packer -S \
 	gcc-multilib \
 	ld-lsb \
 	lib32-qt5-base-bin \
+
+#sudo pacman -S multilib-devel lib32-glib2 lib32-libxext
+#packer -S lib32-qt5-base-bin
+#ln -s /lib64/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
