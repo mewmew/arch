0044ba3437ec604d4ec96795a11daa7102567a7f
mewmew <rnd0x00@gmail.com>
2018-03-24 04:28:41 +0100

    go_get: update tools

--- install/go_get.sh
+++ install/go_get.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+go get -u github.com/mewkiz/cmd/sar
 go get -u github.com/svent/sift
-go get -u github.com/sourcegraph/thyme
 go get -u github.com/goccmack/gocc
 go get -u golang.org/x/tools/...
-go get -u github.com/golang/lint/golint
-go get -u github.com/mrnugget/fzz
+go get -u golang.org/x/lint/...
