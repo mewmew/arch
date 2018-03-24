27e02ecc117dd6ea748e42e1b5f883bd0db95854
mewmew <rnd0x00@gmail.com>
2018-03-24 03:33:14 +0100

    arch: Add arch installation script.

---
+++ install_arch.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Create partions with MBR partition table.
+#
+#    o           - new empty DOS partition table
+#    n p +250M y - new partition 250M /boot
+#    a 1         - toggle bootable flag
+#    n p y       - new partition 100% /
+#    w           - write changes and exit
+echo "Partitioning disk."
+echo -e "o\nn\np\n\n\n+250M\ny\na\n1\nn\np\n\n\n\ny\nw\n" | fdisk /dev/sda
+
+# Format partitions.
+echo "Formatting partitions."
+mkfs.ext2 /dev/sda1
+
+# Create encrypted partitions.
+echo "Creating encrypted partitions."
+cryptsetup -c aes-xts-plain64 -y --use-random luksFormat /dev/sda2
+cryptsetup luksOpen /dev/sda2 luks
+pvcreate /dev/mapper/luks
+vgcreate vg0 /dev/mapper/luks
+lvcreate --size 8G vg0 --name swap
+lvcreate -l +100%FREE vg0 --name root
+mkfs.ext4 /dev/mapper/vg0-root
+mkswap /dev/mapper/vg0-swap
+
+# Mount the filesystems.
+echo "Mounting filesystems."
+mount /dev/mapper/vg0-root /mnt
+swapon /dev/mapper/vg0-swap
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+# Install packages.
+echo "Installing packages."
+pacstrap /mnt base base-devel grub nano wpa_supplicant dialog netctl git wget go
+
+# Generate /etc/fstab.
+echo "Generating /etc/fstab"
+genfstab -pU /mnt >> /mnt/etc/fstab
+# Make /tmp a ramfs.
+echo -e "tmpfs\t/tmp\ttmpfs\tdefaults,noatime,mode=1777\t0\t0" >> /mnt/etc/fstab
+
+# Copy chroot install script.
+echo "Copying chroot install script to chroot."
+cp install_arch_chroot.sh /mnt/root
+
+# Enter the new system.
+echo "Entering chroot."
+arch-chroot /mnt /bin/bash -c /root/install_arch_chroot.sh
+
+rm /mnt/root/install_arch_chroot.sh
+
+# Exit chroot, unmount and reboot/
+#exit
+
+#umount -R /mnt
+#swapoff -a
+
+#reboot
---
+++ install_arch_chroot.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Install sar.
+echo "Installing sar."
+go get github.com/mewkiz/cmd/sar
+export PATH=$HOME/go/bin:$PATH
+
+# Update /etc/fstab
+#
+# * Change to relatime to noatime for all non-boot partitions.
+echo "Updating /etc/fstab"
+sar -i "([ \t]+/[ \t]+ext4[ \t]+rw),relatime," "\${1},noatime," /etc/fstab
+
+# Setup system clock.
+echo "Configuring system clock."
+ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+hwclock --systohc --utc
+
+# Set hostname.
+echo "Setting hostname."
+echo x61s > /etc/hostname
+
+# Update locale
+echo "Updating locale."
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANGUAGE=en_US" >> /etc/locale.conf
+echo "LC_ALL=C" >> /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+
+# Set root password.
+echo "Setting root password."
+passwd
+
+# Add user.
+echo "Adding user."
+useradd -m -g users -G wheel,storage,power,audio u
+passwd u
+
+# Give wheel users sudo permissions.
+echo "Giving wheel users sudo permissions."
+sar -i "[\n]#(%wheel ALL=[(]ALL[)] ALL[\n])" "\n\${1}" /etc/sudoers
+
+# Update /etc/mkinitcpio.conf.
+#
+# * Add 'ext4' to MODULES.
+# * Add 'encrypt' and 'lvm2' to HOOKS before filesystems.
+echo "Updating /etc/mkinitcpio.conf"
+sar -i "[\n]MODULES=[(][)]" "\nMODULES=(ext4)" /etc/mkinitcpio.conf
+sar -i "([\n]HOOKS=[(][^\n]*block) filesystems" "\${1} encrypt lvm2 filesystems" /etc/mkinitcpio.conf
+
+# Regenerate initrd image.
+mkinitcpio -p linux
+
+# Install grub.
+grub-install --recheck /dev/sda
+
+# Update /etc/default/grub.
+#
+# * Add 'cryptdevice=/dev/sdX3:luks:allow-discards' to GRUB_CMDLINE_LINUX.
+sar -i '[\n]GRUB_CMDLINE_LINUX=\"\"' '\nGRUB_CMDLINE_LINUX=\"cryptdevice=/dev/sda2:luks:allow-discards\"' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
