7f5a92a30b920c3b41c9ffb6ba811bc5631b5cc9
mewmew <rnd0x00@gmail.com>
2018-03-24 04:38:51 +0100

    theme: remove fonts no longer present

--- install/theme.sh
+++ install/theme.sh
@@ -16,9 +16,5 @@ sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 wget -O /tmp/local.conf https://gist.githubusercontent.com/cryzed/4f64bb79e80d619866ee0b18ba2d32fc/raw/bd073b52365393f9f0718425271825fc27b218f7/local.conf
 sudo mv /tmp/local.conf /etc/fonts/local.conf
 
-sudo pacman -S \
-	t1-cursor-ib \
-	ttf-gelasio-ib \
-
 packer -S \
 	packer -S fonts-meta-extended-lt \
