d9e58e1baa697f2bad83a776785982ab7fb7bade
mewmew <rnd0x00@gmail.com>
2018-05-09 20:24:03 +0200

    install: update subl comment

--- install/packages.sh
+++ install/packages.sh
@@ -70,12 +70,11 @@ sudo pacman -Sy \
 
 sudo ln -s /usr/share/autojump/autojump.fish /etc/profile.d/autojump.fish
 
-# Add sublime key.
+# Add sublime GPG key and channel.
 wget -O /tmp/sublimehq-pub.gpg https://download.sublimetext.com/sublimehq-pub.gpg
 sudo pacman-key --add /tmp/sublimehq-pub.gpg
 sudo pacman-key --lsign-key 8A8F901A
 rm /tmp/sublimehq-pub.gpg
-
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 
 # Editor
