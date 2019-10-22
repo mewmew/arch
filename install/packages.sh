01aa78f6e8a158fe52433eebfd25e0578fc01425
Robin Eklind <rnd0x00@gmail.com>
2019-10-22 21:17:51 +0200

    install: add multilib to pacman.conf

--- install/packages.sh
+++ install/packages.sh
@@ -7,6 +7,9 @@ sudo pacman-key --lsign-key 8A8F901A
 rm /tmp/sublimehq-pub.gpg
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 
+# Add multilib
+echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+
 # Xorg
 PKGS="mesa-libgl"
 PKGS+=" xclip"
