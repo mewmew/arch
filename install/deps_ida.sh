869c0918f13bd5a0562b4310893808d8d1af56df
Robin Eklind <rnd0x00@gmail.com>
2019-10-22 21:20:35 +0200

    install: sync mirror list before installing packages

--- install/deps_ida.sh
+++ install/deps_ida.sh
@@ -6,6 +6,6 @@ packer -S --noedit \
 #	gcc-multilib \
 #	lib32-qt5-base-bin \
 
-#sudo pacman -S multilib-devel lib32-glib2 lib32-libxext
+#sudo pacman -Sy multilib-devel lib32-glib2 lib32-libxext
 #packer -S --noedit lib32-qt5-base-bin
 #ln -s /lib64/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
--- install/deps_planescape_torment.sh
+++ install/deps_planescape_torment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Planescape Torment dependencies.
-sudo pacman -S \
+sudo pacman -Sy \
 	lib32-alsa-lib \
 	lib32-alsa-plugins \
 	lib32-libpulse \
