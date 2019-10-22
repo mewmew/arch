62add5026e4162e70477568fd7e9c234d2687a4f
Robin Eklind <rnd0x00@gmail.com>
2019-10-22 21:00:24 +0200

    install: update themes, skip confirm when installing meta font package

--- install/themes.sh
+++ install/themes.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Infinality bundle replacement.
-sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
-sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 wget -O /tmp/local.conf https://gist.githubusercontent.com/cryzed/4f64bb79e80d619866ee0b18ba2d32fc/raw/bd073b52365393f9f0718425271825fc27b218f7/local.conf
 sudo mv /tmp/local.conf /etc/fonts/local.conf
 
-packer -S --noedit \
+packer -S --noedit --noconfirm \
 	fonts-meta-extended-lt \
+
+# Infinality bundle replacement.
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/30-infinality-aliases.conf /etc/fonts/conf.d/
