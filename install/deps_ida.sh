9cc38d169e29089628749467a247832059f42882
Robin Eklind <rnd0x00@gmail.com>
2019-10-22 20:53:25 +0200

    install: add --noedit to packer invokations

--- install/deps_ida.sh
+++ install/deps_ida.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-packer -S \
+packer -S --noedit \
 	ld-lsb \
 
 #	gcc-multilib \
 #	lib32-qt5-base-bin \
 
 #sudo pacman -S multilib-devel lib32-glib2 lib32-libxext
-#packer -S lib32-qt5-base-bin
+#packer -S --noedit lib32-qt5-base-bin
 #ln -s /lib64/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
