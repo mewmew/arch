2d44060b9b28067a1dfdd7d69a57e37792cf34b9
Robin Eklind <rnd0x00@gmail.com>
2024-07-08 10:21:56 +0200

    install: remove lensm from go get

--- install/go_get.sh
+++ install/go_get.sh
@@ -6,9 +6,6 @@ go install github.com/mewkiz/cmd/sar@master
 # Rename images based on embedded date.
 go install github.com/mewkiz/cmd/img_date@master
 
-# Visualize asm against Go source code.
-go install loov.dev/lensm@main
-
 # Local HTTP file server.
 go install github.com/mewmew/playground/cmd/dela@master
 
@@ -16,19 +13,16 @@ go install github.com/mewmew/playground/cmd/dela@master
 go install github.com/svent/sift@master
 
 # Wallpaper download utility.
-go install github.com/mewmew/playground/archive/wallhaven/cmd/walls@master
-
-# Word definition lookup.
-go install github.com/Rican7/define@master
+#go install github.com/mewmew/playground/archive/wallhaven/cmd/walls@master
 
 # Go debugger.
-go install github.com/go-delve/delve/cmd/dlv@master
+#go install github.com/go-delve/delve/cmd/dlv@master
 
 # Go tools.
 go install golang.org/x/tools/...@master
 
 # Frontend to Delve.
-go install github.com/aarzilli/gdlv@master
+#go install github.com/aarzilli/gdlv@master
 
 # EBNF parser generator.
 #go install github.com/inspirer/textmapper@master
